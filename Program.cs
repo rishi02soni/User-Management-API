c14132de51dfd5cd39787798f8bbc559885ba01e
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-28 22:33:52 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Data;
+using UserManagement.Repositories;
+using UserManagement.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("UserDb"));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
